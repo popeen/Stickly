b107ef827eb319b1ca3e49eb99a30b70b0966532
Patrik Johansson <patrik@ptjwebben.se>
2024-12-08 00:29:34 +0100

    First version, still very much Alpha as it hasn't been tested for any period of time but it seems to work as intended

---
+++ Stickly/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Stickly;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        noteTextBox = new RichTextBox();
+        SuspendLayout();
+
+        noteTextBox.BackColor = Color.FromArgb(33, 33, 33);
+        noteTextBox.BorderStyle = BorderStyle.None;
+        noteTextBox.Font = new Font("Arial", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        noteTextBox.ForeColor = SystemColors.ControlLightLight;
+        noteTextBox.Location = new Point(12, 12);
+
+        AutoScaleDimensions = new SizeF(10F, 25F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.FromArgb(33, 33, 33);
+        ClientSize = new Size(360, 360);
+        Controls.Add(noteTextBox);
+        ForeColor = SystemColors.ControlLight;
+        FormBorderStyle = FormBorderStyle.SizableToolWindow;
+        Text = "Stickly";
+        ResumeLayout(false);
+    }
+
+    private RichTextBox noteTextBox;
+}
---
+++ Stickly/Form1.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Text.Json;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Stickly
+{
+    public partial class Form1 : Form
+    {
+        private const string saveFile = "stickly.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true; // Set the form to always be on top
+            LoadFormData();
+            noteTextBox.TextChanged += new EventHandler(OnTextChanged);
+            this.FormClosing += new FormClosingEventHandler(OnFormClosing);
+            noteTextBox.Dock = DockStyle.Fill;
+        }
+
+        private void SaveFormData()
+        {
+            try
+            {
+                var formData = new FormData
+                {
+                    Text = noteTextBox.Text,
+                    LocationX = this.Location.X,
+                    LocationY = this.Location.Y,
+                    Width = this.Size.Width,
+                    Height = this.Size.Height
+                };
+
+                var json = JsonSerializer.Serialize(formData);
+                File.WriteAllText(saveFile, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not save the data");
+            }
+        }
+
+        private void LoadFormData()
+        {
+            try
+            {
+                if (File.Exists(saveFile))
+                {
+                    var json = File.ReadAllText(saveFile);
+                    var formData = JsonSerializer.Deserialize<FormData>(json);
+
+                    if (formData != null)
+                    {
+                        noteTextBox.Text = formData.Text;
+
+                        ResizeForm(formData.Width, formData.Height);
+                        MoveForm(formData.LocationX, formData.LocationY);
+
+                        // Check if the last location is fully visible on the screen, if not move it to the top right of the screen
+                        if (!IsFormFullyVisibleOnScreen())
+                        {
+                            MoveFormToDefaultPosition();
+                        }
+                    }
+                }
+                else
+                {
+                    // If there is no config file, move the form to the top right of the screen
+                    MoveFormToDefaultPosition();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error when loading the savefile");
+            }
+        }
+
+        private void ResizeForm(int width, int height)
+        {
+            // Getting the bounds should not be needed according to VS (and copilot) but it is or the form location will not update, even when calling refresh or update methods. Not sure why this is, please let me know if you do.
+            _ = Screen.FromControl(this).Bounds;
+            this.Size = new Size(width, height);
+        }
+        private void MoveForm(int x, int y)
+        {
+            // Getting the bounds should not be needed according to VS (and copilot) but it is or the form location will not update, even when calling refresh or update methods. Not sure why this is, please let me know if you do.
+            _ = Screen.FromControl(this).Bounds;
+            this.Location = new Point(x, y);
+        }
+
+        private void MoveFormToDefaultPosition()
+        {
+            // Top right corner of the screen with some margin to not block the close button of other windows
+            MoveForm(Screen.FromControl(this).Bounds.Right - this.Width - 20, 40);
+        }
+
+        private bool IsFormFullyVisibleOnScreen()
+        {
+            var screenBounds = Screen.FromControl(this).Bounds;
+            var formBounds = new Rectangle(this.Location, this.Size);
+            return screenBounds.Contains(formBounds);
+        }
+
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            SaveFormData();
+        }
+
+        private void OnFormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveFormData();
+        }
+
+    }
+    public class FormData
+    {
+        public string Text { get; set; }
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string FilePath { get; set; }
+
+    }
+
+}
