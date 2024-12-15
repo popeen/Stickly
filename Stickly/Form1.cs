34d3fc5c1a07204bb39bc72c1de002d71b63ea61
Patrik Johansson <patrik@ptjwebben.se>
2024-12-15 01:02:15 +0100

    Added update check to context menu

--- Stickly/Form1.cs
+++ Stickly/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Reflection;
 using System.Text.Json;
 
 namespace Stickly
@@ -21,103 +23,7 @@ namespace Stickly
             {
                 InitializeFileWatcher();
             }
-        }
 
-        private void InitializeFileWatcher()
-        {
-            string directory = AppDomain.CurrentDomain.BaseDirectory; // Get the application's base directory
-            string fullPath = Path.Combine(directory, saveFile); // Combine the base directory with the save file name
-
-            if (Directory.Exists(directory) && File.Exists(fullPath))
-            {
-                fileWatcher = new FileSystemWatcher
-                {
-                    Path = directory,
-                    Filter = Path.GetFileName(saveFile),
-                    NotifyFilter = NotifyFilters.LastWrite
-                };
-
-                fileWatcher.Changed += OnSaveFileChanged;
-                fileWatcher.EnableRaisingEvents = true;
-            }
-        }
-
-        private void OnSaveFileChanged(object sender, FileSystemEventArgs e)
-        {
-            // Reload the form data on a separate thread to avoid cross-thread operation exceptions
-            this.Invoke((MethodInvoker)delegate
-            {
-                LoadFormData();
-            });
-        }
-
-        private void SaveFormData()
-        {
-            try
-            {
-
-                // Temporarily disable the FileSystemWatcher to avoid triggering it when we update the file
-                if (fileWatcher != null)
-                {
-                    fileWatcher.EnableRaisingEvents = false;
-                }
-
-                saveData.Text = noteTextBox.Text;
-                saveData.LocationX = this.Location.X;
-                saveData.LocationY = this.Location.Y;
-                saveData.Width = this.Size.Width;
-                saveData.Height = this.Size.Height;
-
-                var json = JsonSerializer.Serialize(saveData);
-                File.WriteAllText(saveFile, json);
-
-                // Re-enable the FileSystemWatcher
-                if (fileWatcher != null)
-                {
-                    fileWatcher.EnableRaisingEvents = true;
-                }
-
-            }
-            catch
-            {
-                MessageBox.Show("Could not save the data");
-            }
-        }
-
-        public void UpdateStyling()
-        {
-            this.BackColor = ColorTranslator.FromHtml(saveData.Colors.Background);
-            noteTextBox.BackColor = ColorTranslator.FromHtml(saveData.Colors.Background);
-            noteTextBox.ForeColor = ColorTranslator.FromHtml(saveData.Colors.Text);
-            customTitleBar.BackColor = ColorTranslator.FromHtml(saveData.Colors.TitleBarBackground);
-            customTitleBar.ForeColor = ColorTranslator.FromHtml(saveData.Colors.TitleBarText);
-
-            closeButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.CloseButtonBackground);
-            closeButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.CloseButtonText);
-
-            settingsButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.SettingsButtonBackground);
-            settingsButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.SettingsButtonText);
-
-            if (saveData.AlwaysOnTop)
-            {
-                pinButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.PinEnabledButtonBackground);
-                pinButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.PinEnabledButtonText);
-            }
-            else
-            {
-                pinButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.PinDisabledButtonBackground);
-                pinButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.PinDisabledButtonText);
-            }
-
-        }
-
-        private void DrawBoarderOnPaint(object? sender, PaintEventArgs e)
-        {
-            // Draw the custom border
-            using (Pen pen = new Pen(ColorTranslator.FromHtml(saveData.Colors.Border), borderWidth))
-            {
-                e.Graphics.DrawRectangle(pen, new Rectangle(0, 0, this.ClientSize.Width - 1, this.ClientSize.Height - 1));
-            }
         }
 
         private void LoadFormData()
@@ -172,7 +78,112 @@ namespace Stickly
             }
         }
 
-        private void ResizeForm(int width, int height)
+        private void SaveFormData()
+        {
+            try
+            {
+
+                // Temporarily disable the FileSystemWatcher to avoid triggering it when we update the file
+                if (fileWatcher != null)
+                {
+                    fileWatcher.EnableRaisingEvents = false;
+                }
+
+                saveData.Text = noteTextBox.Text;
+                saveData.LocationX = this.Location.X;
+                saveData.LocationY = this.Location.Y;
+                saveData.Width = this.Size.Width;
+                saveData.Height = this.Size.Height;
+
+                var json = JsonSerializer.Serialize(saveData);
+                File.WriteAllText(saveFile, json);
+
+                // Re-enable the FileSystemWatcher
+                if (fileWatcher != null)
+                {
+                    fileWatcher.EnableRaisingEvents = true;
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Could not save the data");
+            }
+        }
+
+        private void InitializeFileWatcher()
+        {
+            string directory = AppDomain.CurrentDomain.BaseDirectory; // Get the application's base directory
+            string fullPath = Path.Combine(directory, saveFile); // Combine the base directory with the save file name
+
+            if (Directory.Exists(directory) && File.Exists(fullPath))
+            {
+                fileWatcher = new FileSystemWatcher
+                {
+                    Path = directory,
+                    Filter = Path.GetFileName(saveFile),
+                    NotifyFilter = NotifyFilters.LastWrite
+                };
+
+                fileWatcher.Changed += OnSaveFileChanged;
+                fileWatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void OnSaveFileChanged(object sender, FileSystemEventArgs e)
+        {
+            // Reload the form data on a separate thread to avoid cross-thread operation exceptions
+            this.Invoke((System.Windows.Forms.MethodInvoker)delegate
+            {
+                LoadFormData();
+            });
+        }
+        private void OnTextChanged(object? sender, EventArgs e)
+        {
+            SaveFormData();
+        }
+
+        private void OnFormClosing(object? sender, FormClosingEventArgs e)
+        {
+            SaveFormData();
+        }
+
+        public void UpdateStyling()
+        {
+            this.BackColor = ColorTranslator.FromHtml(saveData.Colors.Background);
+            noteTextBox.BackColor = ColorTranslator.FromHtml(saveData.Colors.Background);
+            noteTextBox.ForeColor = ColorTranslator.FromHtml(saveData.Colors.Text);
+            customTitleBar.BackColor = ColorTranslator.FromHtml(saveData.Colors.TitleBarBackground);
+            customTitleBar.ForeColor = ColorTranslator.FromHtml(saveData.Colors.TitleBarText);
+
+            closeButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.CloseButtonBackground);
+            closeButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.CloseButtonText);
+
+            settingsButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.SettingsButtonBackground);
+            settingsButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.SettingsButtonText);
+
+            if (saveData.AlwaysOnTop)
+            {
+                pinButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.PinEnabledButtonBackground);
+                pinButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.PinEnabledButtonText);
+            }
+            else
+            {
+                pinButton.BackColor = ColorTranslator.FromHtml(saveData.Colors.PinDisabledButtonBackground);
+                pinButton.ForeColor = ColorTranslator.FromHtml(saveData.Colors.PinDisabledButtonText);
+            }
+
+        }
+
+        private void DrawBoarderOnPaint(object? sender, PaintEventArgs e)
+        {
+            // Draw the custom border
+            using (Pen pen = new Pen(ColorTranslator.FromHtml(saveData.Colors.Border), borderWidth))
+            {
+                e.Graphics.DrawRectangle(pen, new Rectangle(0, 0, this.ClientSize.Width - 1, this.ClientSize.Height - 1));
+            }
+        }
+                private void ResizeForm(int width, int height)
         {
             // Getting the bounds should not be needed according to VS (and copilot) but it is or the form location will not update, even when calling refresh or update methods. Not sure why this is, please let me know if you do.
             _ = Screen.FromControl(this).Bounds;
@@ -208,15 +219,79 @@ namespace Stickly
             return screenBounds.Contains(formBounds);
         }
 
-
-        private void OnTextChanged(object? sender, EventArgs e)
+        private string GetCurrentVersion()
         {
-            SaveFormData();
+            var version = Assembly.GetExecutingAssembly()
+                                  .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                  .InformationalVersion;
+            if (Version.TryParse(version, out Version? parsedVersion))
+            {
+                return parsedVersion.ToString();
+            }
+            return "1.0.0.0"; // If parsing fails, return a dummy version number
         }
 
-        private void OnFormClosing(object? sender, FormClosingEventArgs e)
+        private static async Task<Release> GetLatestVersionAsync()
         {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://api.github.com/repos/popeen/stickly/releases";
+                client.DefaultRequestHeaders.Add("User-Agent", "stickly-updater");
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var releases = JsonSerializer.Deserialize<List<Release>>(jsonResponse);
+
+                return new Release
+                {
+                    Version = releases[0].tag_name,
+                    ChangeLog = releases[0].html_url,
+                    DownloadLink = releases[0].html_url
+                };
+            }
+        }
+
+        private void PinButton_Click(object sender, EventArgs e)
+        {
+            saveData.AlwaysOnTop = !saveData.AlwaysOnTop;
+            this.TopMost = saveData.AlwaysOnTop;
             SaveFormData();
+            UpdateStyling();
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            settingsContextMenu.Show(settingsButton, new Point(0, settingsButton.Height));
+        }
+
+        private async void CheckForUpdates_Click(object sender, EventArgs e)
+        {
+            Release latestVersion = await GetLatestVersionAsync();
+            string version = GetCurrentVersion();
+
+            if (latestVersion == null || string.IsNullOrEmpty(latestVersion.Version) || string.IsNullOrEmpty(version))
+            {
+                MessageBox.Show("Unable to check for updates. Version information is missing.");
+                return;
+            }
+
+            if (new Version(latestVersion.Version).CompareTo(new Version(version)) > 0)
+            {
+                DialogResult result = MessageBox.Show($"There is a new version available.\n\nCurrent version: {version}\nLatest version: {latestVersion.Version}\n\nDo you want to download the new version?", "Update Available", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = latestVersion.DownloadLink,
+                        UseShellExecute = true
+                    });
+                }
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
         private void CustomTitleBar_MouseDown(object sender, MouseEventArgs e)
@@ -224,6 +299,11 @@ namespace Stickly
             isDragging = true;
             lastCursor = Cursor.Position;
             lastForm = this.Location;
+        }
+
+        private void CustomTitleBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
         }
 
         private void CustomTitleBar_MouseMove(object sender, MouseEventArgs e)
@@ -236,36 +316,6 @@ namespace Stickly
             }
         }
 
-        private void CustomTitleBar_MouseUp(object sender, MouseEventArgs e)
-        {
-            isDragging = false;
-        }
-
-        private void CloseButton_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void SettingsButton_Click(object sender, EventArgs e)
-        {
-            settingsContextMenu.Show(settingsButton, new Point(0, settingsButton.Height));
-        }
-        private void CheckForUpdates_Click(object sender, EventArgs e)
-        {
-            // TODO, check for updates
-        }
-
-        private void PinButton_Click(object sender, EventArgs e)
-        {
-            saveData.AlwaysOnTop = !saveData.AlwaysOnTop;
-            this.TopMost = saveData.AlwaysOnTop;
-            SaveFormData();
-            UpdateStyling();
-        }
-
-
-
-        // Override WndProc to handle resizing
         protected override void WndProc(ref Message m)
         {
             const int WM_NCHITTEST = 0x84;
@@ -314,6 +364,7 @@ namespace Stickly
             base.WndProc(ref m);
         }
     }
+
     public class FormData
     {
         public bool AlwaysOnTop { get; set; } = true;
@@ -327,6 +378,7 @@ namespace Stickly
         public string Text { get; set; } = String.Empty;
 
     }
+
     public class Colors
     {
         public string Background { get; set; } = "#212121";
@@ -343,6 +395,16 @@ namespace Stickly
         public string PinDisabledButtonBackground { get; set; } = "#3C3C3F";
         public string PinDisabledButtonText { get; set; } = "#FF636E";
 
+    }
+
+    public class Release
+    {
+        public string Version { get; set; }
+        public string ChangeLog { get; set; }
+        public string DownloadLink { get; set; }
+
+        public string tag_name { get; set; }
+        public string html_url { get; set; }
     }
 
 }
